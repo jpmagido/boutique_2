99a2c873c2c09fcf52c4af49caba8c459c7622ba
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-04 22:18:41 +0200

    modif navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,9 +29,3 @@ $(function() {
   AOS.init();
 
 });
-
-$('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-});
