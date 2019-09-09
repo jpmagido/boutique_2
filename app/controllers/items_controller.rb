6fe166ede7e4ce515cc86ce16331ddb95b3e997b
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-09 17:35:35 +0200

    add images to index & show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,7 @@ class ItemsController < ApplicationController
   	@item_name = Item.find(params[:format]).name
   	@item_price = Item.find(params[:format]).price
   	@item_description = Item.find(params[:format]).description
+  	@item_select = Item.find(params[:format])
   	puts "$"* 100
   	puts params
   	puts "$"* 100
