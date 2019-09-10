3ab6649336f9e177a19a4ac2ceb1f5558b7016a8
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-10 18:50:05 +0200

    update cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -20,14 +20,7 @@ class CartsController < ApplicationController
 
 
 
-  	@items = JtCartItem.where(cart_id: Cart.find_by(user_id: current_user.id)).ids
-
-  	@items_all = JtCartItem.find(@items)
-  	@item_id = JtCartItem.find_by(cart_id: @items).item_id
-  	#@items_all = JtCartItem.where(cart_id: )
-  	@item_img_url= Item.find(@item_id).image_url
-  	@item_name = Item.find(@item_id).name
-  	@item_price = Item.find(@item_id).price
+  	
 
   	puts "$"* 100
   		puts "#{@items}"
