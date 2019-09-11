7c3a751bafd8dbbe8172bf3e5252da402465ccc9
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-11 05:44:27 +0200

    add sum cart function

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,25 +6,10 @@ class CartsController < ApplicationController
 
 	def show
 		@cart_user = Cart.find(params[:format])
-	puts "$"* 100
-  		puts params
-  	puts "$"* 100
 
-
-  	@jt_id = JtCartItem.where(cart_id: Cart.find_by(user_id: current_user.id)).ids
-  	@jt_item_id = JtCartItem.find(@jt_id)
-  	@array_length = @jt_item_id.length - 1
-  	
-
-
-
-
-
-  	
-
-  	puts "$"* 100
-  		puts "#{@items}"
-  	puts "$"* 100
+	  	@jt_id = JtCartItem.where(cart_id: Cart.find_by(user_id: current_user.id)).ids
+	  	@jt_item_id = JtCartItem.find(@jt_id)
+	  	@array_length = @jt_item_id.length - 1  	
 	end
 
 
--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,2 +1,21 @@
 module CartHelper
+
+	def total_price
+
+	@jt_id = JtCartItem.where(cart_id: Cart.find_by(user_id: current_user.id)).ids
+  	@jt_item_id = JtCartItem.find(@jt_id)
+  	@array_length = @jt_item_id.length - 1
+  	i = 0
+	  	while @array_length >= 0
+	  		i += (Item.find(@jt_item_id[@array_length][:item_id]).price)
+	  		@array_length -= 1		
+	  	end
+		return i
+	end
+
+	def total_price_ttc
+
+		total_price * 1.2
+		
+	end
 end
