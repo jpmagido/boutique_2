35c9a55b8ce415f78820d991d7345ac62faa6fd9
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-11 05:04:49 +0200

    add delete method on cart items

--- app/controllers/jt_cart_items_controller.rb
+++ app/controllers/jt_cart_items_controller.rb
@@ -2,10 +2,19 @@ class JtCartItemsController < ApplicationController
 
 	def create
 
-		JtCartItem.create(cart_id: Cart.find_by(user_id: current_user.id).id, item_id: params[:format])
+		JtCartItem.create(cart_id: Cart.find_by(user_id: current_user.id).id, item_id: params[:format])		
+		redirect_to(carts_path(Cart.find_by(user_id: current_user.id).id))
+	end
 
+	def destroy
+
+		@cart_select = Cart.find(params[:format])
+		@private_cart = JtCartItem.find_by(cart_id: @cart_select)
 		
+		@private_cart.destroy
 		
 		redirect_to(carts_path(Cart.find_by(user_id: current_user.id).id))
 	end
+
+	
 end
