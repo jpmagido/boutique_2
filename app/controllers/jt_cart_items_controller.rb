537e0f63dfff309a4396986492348d99b6ec11b2
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-11 05:18:10 +0200

    add links cart & delete buttons

--- app/controllers/jt_cart_items_controller.rb
+++ app/controllers/jt_cart_items_controller.rb
@@ -10,7 +10,6 @@ class JtCartItemsController < ApplicationController
 
 		@cart_select = Cart.find(params[:format])
 		@private_cart = JtCartItem.find_by(cart_id: @cart_select)
-		
 		@private_cart.destroy
 		
 		redirect_to(carts_path(Cart.find_by(user_id: current_user.id).id))
