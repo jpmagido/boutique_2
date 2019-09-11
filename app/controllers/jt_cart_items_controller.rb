f72cb9493a3f90a5fbc30f6209a2da6b802f3be6
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-11 08:24:41 +0200

    fix delete method

--- app/controllers/jt_cart_items_controller.rb
+++ app/controllers/jt_cart_items_controller.rb
@@ -8,9 +8,9 @@ class JtCartItemsController < ApplicationController
 
 	def destroy
 
-		@cart_select = Cart.find(params[:format])
-		@private_cart = JtCartItem.find_by(cart_id: @cart_select)
-		@private_cart.destroy
+		@private_cart = JtCartItem.where(cart_id: Cart.find_by(user_id: current_user.id).id, item_id: params[:format]).ids
+
+		JtCartItem.find(@private_cart[0]).destroy
 		
 		redirect_to(carts_path(Cart.find_by(user_id: current_user.id).id))
 	end
