b7866a087f196e49a7a929c554bbfb22c054603e
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-16 17:32:53 +0200

    create Order db

--- db/migrate/20190816161850_create_orders.rb
+++ db/migrate/20190816161850_create_orders.rb
@@ -1,7 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.integer :price
+      t.integer :total_price
+      t.string :adress
+      t.string :zip_code
+      t.string :country
+      t.string :first_name
+      t.string :last_name
+      t.string :company_name
+
       t.belongs_to :user, index: true
 
       t.timestamps
--- db/migrate/20190816161921_create_jt_order_items.rb
+++ db/migrate/20190816161921_create_jt_order_items.rb
@@ -2,7 +2,7 @@ class CreateJtOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :jt_order_items do |t|
       t.belongs_to :order, index: true
-      t.belongs_to :item, index: true
+      t.belongs_to :jt_cart_items, index: true
 
       t.timestamps
     end
