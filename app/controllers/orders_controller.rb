e36f2a465ce882ba500f2fdb1cca2d3f8d98cc42
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-17 09:56:28 +0200

    add products in orders view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,9 @@
 class OrdersController < ApplicationController
+
+	def show
+
+	  	@jt_id = JtCartItem.where(cart_id: Cart.find_by(user_id: current_user.id)).ids
+	  	@jt_item_id = JtCartItem.find(@jt_id)
+	  	@array_length = @jt_item_id.length - 1 
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
   get 'static_pages/thank_you'
   root 'static_pages#home'
 
-  resources :items, :carts, :jt_cart_items, :charges, :orders
+  resources :items, :carts, :jt_cart_items, :charges
+  resource :orders
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
