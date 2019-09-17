a1465e44bc63ab29869c6cc80dacb16abbd23885
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-17 10:08:53 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,9 @@ Rails.application.routes.draw do
   get 'carts/index'
   
   devise_for :users
-  get 'static_pages/index'
   get 'static_pages/secret'
   get 'static_pages/contact'
-  get 'static_pages/checkout'
-  get 'static_pages/cart'
   get 'static_pages/about'
-  get 'static_pages/shop'
-  get 'static_pages/shop_single'
   get 'static_pages/thank_you'
   root 'static_pages#home'
 
