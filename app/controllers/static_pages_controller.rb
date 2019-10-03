588d4fee1575c0ae767b346db37cf85328bcd286
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-10-03 15:08:29 +0200

    Create secret_page for User

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,9 @@ class StaticPagesController < ApplicationController
   end
 
   def secret
+  	@mail_user = User.find(current_user.id).email
+  	@id_user = current_user.id
+  	@factu_user = Order.where(user_id: @id_user).last
   end
 
   def home
